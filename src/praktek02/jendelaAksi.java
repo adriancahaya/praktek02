5eeb35cd44791f88c3f60b76bf6c9b754210fdfd
adriancahaya <32443085+adriancahaya@users.noreply.github.com>
2018-03-30 22:06:33 +0800

    mengisi nilai atribute

--- src/praktek02/jendelaAksi.java
+++ src/praktek02/jendelaAksi.java
@@ -3,6 +3,8 @@ package praktek02;
 public class jendelaAksi {
     public static void main(String[] args) {
         Jendela r1 = new Jendela();
+        r1.panjang = 5;
+        r1.lebar = 9;
     }
             
 }
