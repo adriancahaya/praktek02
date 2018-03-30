2ec9730409f0b2acdfe824386554079545385dee
adriancahaya <32443085+adriancahaya@users.noreply.github.com>
2018-03-30 21:55:34 +0800

    membuat atribut dan behavior

--- src/praktek02/Jendela.java
+++ src/praktek02/Jendela.java
@@ -1,5 +1,17 @@
 
 package praktek02;
 public class Jendela {
+    double panjang;
+    double lebar;
     
+    void cetakinfo(){
+          System.out.println("====================");
+          System.out.println("panjang :"+panjang);
+          System.out.println("lebar :"+lebar);
+          System.out.println("====================");
+    }
+    
+
+
 }
+
